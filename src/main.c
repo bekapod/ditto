653716f4a9ea25cc3f883c7bab246290d53ebac2
root <becky@bekapod.dev>
2026-09-13 14:45:57 +0000

    feat(state): adopt stacked game states

--- src/main.c
+++ src/main.c
@@ -2,20 +2,7 @@
 #include <gbdk/platform.h>
 
 #include "audio.h"
-#include "bg.h"
-#include "blink.h"
-#include "fade.h"
-#include "flash.h"
-#include "input.h"
-#include "main.h"
-#include "music.h"
-#include "rng.h"
-#include "save.h"
-#include "seq.h"
-#include "shake.h"
-#include "sfx.h"
-#include "text.h"
-#include "vblank.h"
+#include "pallet.h"
 
 extern const hUGESong_t placeholder;
 extern const uint8_t font_tiles[];
@@ -23,24 +10,50 @@ extern const uint8_t font_tiles[];
 static const uint8_t empty_background[32 * 32] = {0};
 
 #define FADE_FRAMES_PER_STEP 4
+#define DITTO_SAVE_VERSION 1U
 
-uint8_t state = STATE_TITLE_INIT;
-uint16_t run_seed;
+typedef struct {
+    uint8_t version;
+    uint16_t score;
+} save_t;
+
+static uint16_t run_seed;
 static blink_state_t title_prompt;
 static uint16_t score;
 static save_t save_data;
 static uint8_t run_seed_captured;
 
-void title_init(void) {
+static void title_init(void);
+static void title_update(void);
+static void play_wait_update(void);
+static void play_init(void);
+static void play_update(void);
+static void pause_init(void);
+static void pause_update(void);
+static void pause_exit(void);
+
+static const state_t title_state = {
+    title_init, title_update, 0, 0, 0
+};
+static const state_t play_wait_state = {
+    0, play_wait_update, 0, 0, 0
+};
+static const state_t play_state = {
+    play_init, play_update, 0, 0, 0
+};
+static const state_t pause_state = {
+    pause_init, pause_update, 0, pause_exit, 1
+};
+
+static void title_init(void) {
     save_data.version = DITTO_SAVE_VERSION;
     if (!save_load(&save_data, sizeof(save_data)))
         save_data.version = DITTO_SAVE_VERSION;
     blink_init(&title_prompt);
     text_print(8, 8, "PUSH START");
-    state = STATE_TITLE;
 }
 
-void title_update(void) {
+static void title_update(void) {
     blink_tick(&title_prompt);
     if (title_prompt.dirty) {
         text_print(8, 8, title_prompt.visible ? "PUSH START" : "          ");
@@ -54,14 +67,16 @@ void title_update(void) {
         rng_init(&rng_global, run_seed);
         run_seed_captured = 1;
         fade_out(FADE_FRAMES_PER_STEP);
-        state = STATE_PLAY_INIT;
+        state_replace(&play_wait_state);
     }
 }
 
-void play_init(void) {
-    if (fade_active)
-        return;
+static void play_wait_update(void) {
+    if (!fade_active)
+        state_replace(&play_state);
+}
 
+static void play_init(void) {
     score = save_data.score;
     DISPLAY_OFF;
     set_bkg_tiles(0, 0, 32, 32, empty_background);
@@ -69,12 +84,19 @@ void play_init(void) {
     text_digits(14, 8, score, 5);
     DISPLAY_ON;
     fade_in(FADE_FRAMES_PER_STEP);
-    state = STATE_PLAY_FADE_IN;
 }
 
-void play_fade_in(void) {
-    if (!fade_active)
-        state = STATE_PLAY;
+static void pause_init(void) {
+    text_print(8, 10, "PAUSED");
+}
+
+static void pause_update(void) {
+    if (input_pressed & J_START)
+        state_pop();
+}
+
+static void pause_exit(void) {
+    text_print(8, 10, "      ");
 }
 
 static void play_flash(void) {
@@ -85,7 +107,13 @@ static void play_shake(void) {
     shake(6, 2);
 }
 
-void play_update(void) {
+static void play_update(void) {
+    if (fade_active || (input_pressed & J_START)) {
+        if (!fade_active && (input_pressed & J_START))
+            state_push(&pause_state);
+        return;
+    }
+
     if (seq_busy())
         return;
 
@@ -115,30 +143,13 @@ void main(void) {
     add_VBL(pallet_vblank_tick);
     add_VBL(hUGE_dosound);
     music_play(&placeholder);
+    state_push(&title_state);
 
     while (1) {
+        input_update();
+        state_tick();
+        seq_tick();
         vsync();
         bg_flush();
-        input_update();
-        seq_tick();
-        text_vblank();
-
-        switch (state) {
-        case STATE_TITLE_INIT:
-            title_init();
-            break;
-        case STATE_TITLE:
-            title_update();
-            break;
-        case STATE_PLAY_INIT:
-            play_init();
-            break;
-        case STATE_PLAY_FADE_IN:
-            play_fade_in();
-            break;
-        case STATE_PLAY:
-            play_update();
-            break;
-        }
     }
 }
