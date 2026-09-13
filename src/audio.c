6ac60f0e8f22eeaf875488614457562514ecea0f
Becky <becky@bekapod.dev>
2026-09-13 21:48:55 +0100

    refactor: use SFX_ prefixed audio macros

--- src/audio.c
+++ src/audio.c
@@ -2,10 +2,10 @@
 #include "sfx.h"
 
 static const sfx_row_t test_sfx_rows[] = {
-    {0, SFX_REG_PULSE2_DUTY, DUTY_12_5},
-    {0, SFX_REG_PULSE2_ENV, ENV(12, 2)},
-    {0, SFX_REG_PULSE2_FREQ_LO, PITCH_LO(1200)},
-    {6, SFX_REG_PULSE2_FREQ_HI, PITCH_HI(1200) | SFX_TRIGGER},
+    {0, SFX_REG_PULSE2_DUTY, SFX_DUTY_12_5},
+    {0, SFX_REG_PULSE2_ENV, SFX_ENV(12, 2)},
+    {0, SFX_REG_PULSE2_FREQ_LO, SFX_PITCH_LO(1200)},
+    {6, SFX_REG_PULSE2_FREQ_HI, SFX_PITCH_HI(1200) | SFX_TRIGGER},
     {SFX_END, 0, 0},
 };
 
